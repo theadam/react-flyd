c839d66fe63b0f4f269d5870305be5d3ba35b106
Adam Nalisnick <theadam4257@gmail.com>
2016-01-12 23:23:03 -0500

    Style changes

--- src/hyperscript.js
+++ src/hyperscript.js
@@ -4,15 +4,15 @@ import { reactive } from 'react-flyd-class';
 
 import { hasStream } from './utils';
 
-function wrapChildren(children){
+function wrapChildren(children) {
   return children.map(child => {
-    if(!isStream(child)) return child;
+    if (!isStream(child)) return child;
     return createElement(reactive('span'), {}, child);
   });
 }
 
-export function h(tag, props, ...children){
-  if(hasStream(props) || props && isStream(props.mount)){
+export function h(tag, props, ...children) {
+  if (hasStream(props) || props && isStream(props.mount)) {
     return createElement(
       reactive(tag),
       props,
