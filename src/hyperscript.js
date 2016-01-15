297eea80093b79bb37fc483dccfc66423493252a
Adam Nalisnick <theadam4257@gmail.com>
2016-01-15 09:28:16 -0500

    Handled nested props

--- src/hyperscript.js
+++ src/hyperscript.js
@@ -2,7 +2,7 @@ import { isStream } from 'flyd';
 import { createElement } from 'react';
 import { reactive } from 'react-flyd-class';
 
-import { hasStream } from './utils';
+import { hasStream, toStreams } from './utils';
 
 function wrapChildren(children) {
   return children.map(child => {
@@ -11,11 +11,12 @@ function wrapChildren(children) {
   });
 }
 
+
 export function h(tag, props, ...children) {
   if (hasStream(props) || props && isStream(props.mount)) {
     return createElement(
       reactive(tag),
-      props,
+      toStreams(props),
       ...(children.length === 1 ? children : wrapChildren(children))
     );
   }
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import { isStream } from 'flyd';
+import { stream as objStream } from 'flyd/module/obj';
 
-const isPlainObject = (obj) => typeof obj === 'object' && obj.constructor === Object;
+const isPlainObject = (obj) => obj !== null && typeof obj === 'object' && Object.getPrototypeOf(obj) === Object.prototype;
 
 export function hasStream(obj) {
   return Object.keys(obj || {}).reduce((hasOne, key) => {
@@ -10,4 +11,12 @@ export function hasStream(obj) {
     if (isPlainObject(val)) return hasStream(val);
     return hasOne;
   }, false);
+}
+
+function wrap(obj) {
+  return isPlainObject(obj) && hasStream(obj) ? objStream(obj) : obj;
+}
+
+export function toStreams(props) {
+  return Object.keys(props).reduce((acc, key) => ({...acc, [key]: wrap(props[key])}), {});
 }
