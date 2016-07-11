fb97115822aa0c3ad8fab5ba2ce0f618f3f38d1e
xialvjun <xialvjun@live.com>
2016-07-11 15:14:02 +0800

    for issue https://github.com/theadam/react-flyd/issues/1#issuecomment-231657914

--- src/hyperscript.js
+++ src/hyperscript.js
@@ -1,24 +1,44 @@
-import { isStream } from 'flyd';
-import { createElement } from 'react';
+import { isStream, combine } from 'flyd';
+import { createElement, isValidElement } from 'react';
 import { reactive } from 'react-flyd-class';
 
 import { hasStream, toStreams } from './utils';
 
 function wrapChildren(children) {
-  return children.map(child => {
-    if (!isStream(child)) return child;
-    return createElement(reactive('span'), {}, child);
-  });
+  const notValidElement$s = children.filter(child => isStream(child) && !isValidElement(child()));
+  if (notValidElement$s.length>0) {
+    return combine(() => {
+      return children.map(child => {
+        if (!isStream(child)) {
+          return child;
+        } else if (notValidElement$s.indexOf(child) > -1) {
+          return child();
+        } else {
+          return createElement(reactive(), {}, child);
+        }
+      });
+    }, notValidElement$s);
+  } else {
+    return children.map(child => {
+      if (!isStream(child)) {
+        return child;
+      } else {
+        return createElement(reactive(), {}, child);
+      }
+    })
+  }
 }
 
 
 export function h(tag, props, ...children) {
-  if (hasStream(props) || props && isStream(props.mount)) {
-    return createElement(
-      reactive(tag),
-      toStreams(props),
-      ...(children.length === 1 ? children : wrapChildren(children))
-    );
+  props = props || {}
+  children = wrapChildren(children)
+  if (hasStream(props) || props && isStream(props.mount) || isStream(children)) {
+    if (isStream(children)) {
+      return createElement(reactive(tag), toStreams(props), children);
+    } else {
+      return createElement(reactive(tag), toStreams(props), ...children);
+    }
   }
-  return createElement(tag, props, ...wrapChildren(children));
+  return createElement(tag, props, ...children);
 }
