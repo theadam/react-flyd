a2edaec6c3aa86ad0905801e258effd75b23dbb5
xialvjun <xialvjun@live.com>
2016-07-14 03:04:15 +0800

    eslint

--- src/hyperscript.js
+++ src/hyperscript.js
@@ -6,39 +6,40 @@ import { hasStream, toStreams } from './utils';
 
 function wrapChildren(children) {
   const notValidElement$s = children.filter(child => isStream(child) && !isValidElement(child()));
-  if (notValidElement$s.length>0) {
-    return combine(() => {
+  let wrappedChildren;
+  if (notValidElement$s.length > 0) {
+    wrappedChildren = [combine(() => {
       return children.map(child => {
+        let element;
         if (!isStream(child)) {
-          return child;
+          element = child;
         } else if (notValidElement$s.indexOf(child) > -1) {
-          return child();
+          element = child();
         } else {
-          return createElement(reactive(), {}, child);
+          element = createElement(reactive(), {}, child);
         }
+        return element;
       });
-    }, notValidElement$s);
+    }, notValidElement$s)];
   } else {
-    return children.map(child => {
+    wrappedChildren = children.map(child => {
+      let element;
       if (!isStream(child)) {
-        return child;
+        element = child;
       } else {
-        return createElement(reactive(), {}, child);
+        element = createElement(reactive(), {}, child);
       }
-    })
+      return element;
+    });
   }
+  return wrappedChildren;
 }
 
-
 export function h(tag, props, ...children) {
-  props = props || {}
-  children = wrapChildren(children)
-  if (hasStream(props) || props && isStream(props.mount) || isStream(children)) {
-    if (isStream(children)) {
-      return createElement(reactive(tag), toStreams(props), children);
-    } else {
-      return createElement(reactive(tag), toStreams(props), ...children);
-    }
+  let defaultProps = props || {};
+  let wrappedChildren = wrapChildren(children);
+  if (hasStream(defaultProps) || defaultProps && isStream(defaultProps.mount) || hasStream(wrappedChildren)) {
+    return createElement(reactive(tag), toStreams(defaultProps), ...wrappedChildren);
   }
-  return createElement(tag, props, ...children);
+  return createElement(tag, defaultProps, ...children);
 }
