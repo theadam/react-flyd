c708e6432f14d93b14cdf4e26849b1a773342c49
xialvjun <xialvjun@live.com>
2016-07-15 19:22:18 +0800

    props needn't be toStream() delete props.mount

--- src/hyperscript.js
+++ src/hyperscript.js
@@ -2,41 +2,38 @@ import { isStream, combine } from 'flyd';
 import { createElement, isValidElement } from 'react';
 import { reactive } from 'react-flyd-class';
 
-import { hasStream, toStreams } from './utils';
+function hasStream(obj) {
+  return Object.keys(obj).some(key => isStream(obj[key]));
+}
 
 function wrapChildren(children) {
   const notValidElement$s = children.filter(child => isStream(child) && !isValidElement(child()));
   if (notValidElement$s.length > 0) {
     return [combine(() => {
       return children.map(child => {
-        let element;
         if (!isStream(child)) {
-          element = child;
-        } else if (notValidElement$s.indexOf(child) > -1) {
-          element = child();
-        } else {
-          element = createElement(reactive(), {}, child);
+          return child;
         }
-        return element;
+        if (notValidElement$s.indexOf(child) > -1) {
+          return child();
+        }
+        return createElement(reactive(), {}, child);
       });
     }, notValidElement$s)];
   }
   return children.map(child => {
-    let element;
     if (!isStream(child)) {
-      element = child;
-    } else {
-      element = createElement(reactive(), {}, child);
+      return child;
     }
-    return element;
+    return createElement(reactive(), {}, child);
   });
 }
 
 export function h(tag, props, ...children) {
   let defaultProps = props || {};
   let wrappedChildren = wrapChildren(children);
-  if (hasStream(defaultProps) || defaultProps && isStream(defaultProps.mount) || hasStream(wrappedChildren)) {
-    return createElement(reactive(tag), toStreams(defaultProps), ...wrappedChildren);
+  if (hasStream(defaultProps) || hasStream(wrappedChildren)) {
+    return createElement(reactive(tag), defaultProps, ...wrappedChildren);
   }
   return createElement(tag, defaultProps, ...wrappedChildren);
 }
